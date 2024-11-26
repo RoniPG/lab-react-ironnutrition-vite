64108c648b8fd28bc9a22a179ec3ce1f4beeebb9
RoniPG <ronigmn@gmail.com>
2024-11-26 17:12:18 +0100

    Iteration 4 | Delete List Items

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,16 @@ import FoodBox from "./components/FoodBox";
 
 function App() {
   const [foods, setFoods] = useState(foodJson);
-
+  const handleDelete = (event) => {
+    const filterfoods = foods.filter((food) => (
+      food.id !== event.target.id
+    ));
+    setFoods(filterfoods);
+  };
   return (
     <div className="App">
       <h1 className="mb-5">LAB | React IronNutrition</h1>
-      <FoodBox foods={foods} />
+      <FoodBox foods={foods} handleDelete={handleDelete} />
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,17 +1,17 @@
 // Your code here
-function FoodBox({ foods }) {
+function FoodBox({ foods, handleDelete }) {
     return (
         <div>
             {foods.map((food) => (
                 <div key={food.id} className="container border border-black d-flex flex-column align-items-center mt-1 p-3">
                     <p className="fs-1 fw-bold">{food.name}</p>
-                    <img width="350px" src={food.image}/>
+                    <img width="350px" src={food.image} />
                     <p>Calories: {food.calories}</p>
                     <p>Servings <b>{food.servings}</b></p>
                     <p>
                         <b>Total Calories: {food.servings * food.calories} </b> kcal
                     </p>
-                    <button>Delete</button>
+                    <button id={food.id} onClick={(event) => handleDelete(event)}>Delete</button>
                 </div>
             ))}
 
