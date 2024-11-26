9d437c353ea8ca1f7ead1d0be53b90ffc6b91ff1
RoniPG <ronigmn@gmail.com>
2024-11-26 13:37:51 +0100

    Iteration 1 | Store Data in the State

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import "./App.css";
+import foodJson from "./foods.json"
 
 function App() {
+  const [foods, setFoods] = useState(foodJson);
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
